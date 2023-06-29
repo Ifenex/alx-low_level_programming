ecc36092d78f331de189fcec3a498dbd6668f4f6
Ifenex <ogbuifeanyi57@gmail.com>
2023-06-29 14:52:04 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncat - concatenates two strings.
+ * @dest: first string
+ * @src: second string
+ * Return: point
+ * @n: null
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	a = 0;
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (b < n && src[a] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++
+	}
+	dest[a] = '\0';
+	return (dest);
